7af02ea47aa8732b87300a63d1d9bf82464b5f1d
rorixrebel <elxilote@gmail.com>
2019-06-12 21:45:15 -0500

    updating

--- fish/config.fish
+++
@@ -1,3 +0,0 @@
-source ~/dotfiles/.aliases
-#export PATH=~/.npm-global/bin:$PATH
-set -G EDITOR='nvim'
--- syncfiles.sh
+++ syncfiles.sh
@@ -1,24 +1,12 @@
-#!/usr/bin/fish
+#!/usr/bin/zsh
 # Sync dotfiles
-
-# i3
-rsync ~/.config/i3/config ~/dotfiles/i3/config
-echo "I3 in sync"
 
 # vimrc
 rsync ~/.config/nvim/init.vim ~/dotfiles/nvim/init.vim
 echo "Nvim in sync"
 
-# ranger
-rsync ~/.config/ranger/rc.conf ~/dotfiles/ranger/rc.conf
-echo "ranger in sync"
-
-# fish
-rsync ~/.config/fish/ ~/dotfiles/fish/
-echo "fish in sync"
-
-# alacritty
-rsync ~/.config/alacritty/alacritty.yml ~/dotfiles/alacritty
-echo "alacritty in sync"
+# zshrc
+rsync ~/.zshrc ~/dotfiles/zsh/zshrc
+echo "zshrc in sync"
 
 

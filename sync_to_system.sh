64a41b54c129ed43d996334e9dc01a9953a3e0ab
rorixrebel <elxilote@gmail.com>
2019-06-20 20:36:10 -0500

    updated files

--- sync_to_system.sh
+++ sync_to_system.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/fish
 # i3
-rsync ~/dotfiles/i3/config ~/.config/i3/config 
-echo "I3 in sync"
+#rsync ~/dotfiles/i3/config ~/.config/i3/config 
+#echo "I3 in sync"
 
 # vimrc
 rsync ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim 
 echo "Nvim in sync"
 
-# termite
-rsync ~/dotfiles/termite/config ~/.config/termite/config 
-echo "Termite in Sync"
-
-# ranger
-rsync  ~/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
-echo "ranger in sync"
-
-# fish
-rsync ~/dotfiles/fish/ ~/.config/fish/
+rsync ~/dotfiles/zsh/.zshrc ~/.zshrc
+echo "zshrc in sync"

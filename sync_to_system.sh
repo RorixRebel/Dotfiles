ed6b3dfd29354f4c60ad6c6fd19e09f2b57be3a2
rorix <elxilote@gmail.com>
2019-08-17 09:40:39 -0500

    updating for laptop

--- sync_to_system.sh
+++ sync_to_system.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/fish
+#!/usr/bin/zsh
 # i3
-#rsync ~/dotfiles/i3/config ~/.config/i3/config 
-#echo "I3 in sync"
+rsync ~/dotfiles/i3/config ~/.config/i3/config 
+echo "I3 in sync"
 
 # vimrc
 rsync ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim 
--- syncfiles.sh
+++ syncfiles.sh
@@ -9,4 +9,11 @@ echo "Nvim in sync"
 rsync ~/.zshrc ~/dotfiles/zsh/zshrc
 echo "zshrc in sync"
 
+# kde
+rsync ~/.config/plasma-workspace/env ~/dotfiles/kde
+echo "kde in sync"
+
+# i3
+rsync ~/.config/i3/config  ~/dotfiles/i3
+echo "i3 in sync"
 
